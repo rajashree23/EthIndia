543a91f8bf343e3ef01b42dcf81b1851fe339990
frontend.appstone <frontend.appstone@gmail.com>
2020-06-15 20:44:58 +0530

    changes done

--- client/src/components/loader.js
+++ client/src/components/loader.js
@@ -5,14 +5,21 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import CircularProgress from '@material-ui/core/CircularProgress';
-
+import chainWizImage from "./BG2.png";
+const chainWiz = {
+  backgroundImage: "url(" + chainWizImage + ")",
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  marginTop: -35,
+  marginLeft: -10
+}
 export default class Loader extends React.Component {
 
   render() {
 
     return (
       <Dialog
-        style={{ backgroundColor: "#E6F9F9" }}
+        style={chainWiz}
         open={true}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
